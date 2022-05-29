50df27ed8592009eb68bb6315652f7a5229dae2b
StevenNimble <stadenoia@gmail.com>
2022-05-28 23:09:22 -0500

    Adding file viewing page

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 # import necessary libraries
+# from tkinter import image_names
 from flask import Flask, render_template, request
 import os
 from os.path import join, dirname, realpath
+from pathlib import Path
 
 # create instance of Flask app
 app = Flask(__name__)
@@ -13,21 +15,34 @@ def index():
     return render_template("index.html", 
     text="Serving up cool text from the Flask server!!")
 
-@app.route("/file")
+# Show a list of images.
+@app.route("/files")
 def file():
-    return render_template("file.html", 
-    text="File page")
+    # image_names = join(dirname(realpath(__file__)), 'static/uploads')
+    # return render_template("files.html", image_name = image_names)
+    basepath = f"static/uploads"
+    dir = os.walk(basepath)
+    file_list = []
 
+    for path, files in dir:
+        for file in files:
+            temp = path.joinPath(path + '/', file)
+            file_list.append(temp)
+            print(file_list + "hello")
+    return render_template('files.html', hists=file_list)
+
+# Route for displaying and saving uploaded image
 @app.route('/upload-image', methods=['GET', 'POST'])
 def upload_image():
     if request.method == "POST":
         if request.files:
             image = request.files["image"]
             image.save(os.path.join(app.config["UPOADED_IMAGE"], image.filename))
-            return render_template("file.html", uploaded_image=image.filename)
-    return render_template("file.html")
+            return render_template("upload.html", uploaded_image=image.filename)
+    return render_template("upload.html")
 
 
+# Route for displaying uploaded picture
 @app.route('/uploads/<filename>')
 def send_uploaded_file(filename=''):
     from flask import send_from_directory
