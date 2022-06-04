fedf2f99a12e4c3c93d80e611094024502d9a85a
StevenMain <merge.conflictstyle>
2022-06-04 13:17:16 -0500

    adding upload to index incomplete

--- app.py
+++ app.py
@@ -10,9 +10,14 @@ app = Flask(__name__)
 app.config['UPOADED_IMAGE'] = join(dirname(realpath(__file__)), 'static/uploads/..')
 
 # create route that renders index.html template
-@app.route("/")
-def index():
-    return render_template("index.html")
+@app.route("/upload", methods=['GET', 'POST'])
+def upload_image():
+    if request.method == "POST":
+        if request.files:
+            image = request.files["image"]
+            image.save(os.path.join(app.config["UPOADED_IMAGE"], image.filename))
+            return render_template("upload.html", uploaded_image=image.filename)
+    return render_template("upload.html")
 
 # Show a list of images.
 @app.route("/files")
@@ -31,14 +36,14 @@ def file():
     return render_template('files.html', hists=file_list)
 
 # Route for displaying and saving uploaded image
-@app.route('/upload-image', methods=['GET', 'POST'])
-def upload_image():
+@app.route('/', methods=['GET', 'POST'])
+def index():
     if request.method == "POST":
         if request.files:
             image = request.files["image"]
             image.save(os.path.join(app.config["UPOADED_IMAGE"], image.filename))
-            return render_template("upload.html", uploaded_image=image.filename)
-    return render_template("upload.html")
+            return render_template("index.html", uploaded_image=image.filename)
+    return render_template("index.html")
 
 
 # Route for displaying uploaded picture
